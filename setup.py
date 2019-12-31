b298dbd6db270ee020c0189ad88dcb0b65cd5569
isaac47 <isaac.yimgaing@gmail.com>
2019-12-31 11:22:49 +0100

    Update setup;py

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'pyparallelizer',  
+  packages = ['pyparallelizer'],  
+  version = '0.1',  
+  license='MIT', 
+  description = 'TYPE YOUR DESCRIPTION HERE',   
+  author = 'Isaac YIMGAINGE',            
+  author_email = 'isaac.yimgaing@gmail.com',     
+  url = 'https://github.com/isaac47/pyparallelizer',
+  download_url = 'https://github.com/isaac47/pyparallelizer/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['PYTHON', 'DATAFRAME', 'MULTIPROCESSING'],   
+  install_requires=[            
+          'pandas',
+          'numpy',
+          'itertools',
+          'multiprocessing',,
+          'tqdm'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',     
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
 
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pyparallelizer",
-    version="1.0.0",
-    author="Isaac YIMGAING",
-    author_email="isaac.yimgaing@gmail.com",
-    description="A package that makes it easy to create Pypi packages",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="",
-    keywords = ['PYTHON', 'DATAFRAME', 'MULTIPROCESSING'],
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-) 
